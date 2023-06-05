d45dbf0db5f0c0b41d7bc5adf0a260148bfd15b7
Александр Ильин <sasha8@list.ru>
2023-06-05 10:19:37 +0300

    Добавьте файлы проекта.

---
+++ Многопоточность/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int x = 0;
+object locker = new();  // объект-заглушка
+
+// запускаем пять потоков:
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new(Print);
+    myThread.Name = $"Поток {i}";
+    myThread.Start();
+}
+
+void End(string stroka)
+{
+    Console.WriteLine(stroka);
+}
+
+void Print()
+{
+    lock (locker)
+    {
+        //x = 1;
+        for (int i = 1; i < 6; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            x++;
+            Thread.Sleep(100);
+        }
+        End("Выполнено");
+    }
+}
